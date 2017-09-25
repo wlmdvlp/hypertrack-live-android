1e3383cb8ed21961b44f39e97cafb56838de3c09
Aman <aman@hypertrack.io>
2017-09-25 23:22:55 +0530

    Adds RefWatcher to watch Home.java

--- app/src/main/java/io/hypertrack/sendeta/MyApplication.java
+++ app/src/main/java/io/hypertrack/sendeta/MyApplication.java
@@ -25,12 +25,14 @@ SOFTWARE.
 package io.hypertrack.sendeta;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 import com.hypertrack.lib.HyperTrack;
 import com.hypertrack.lib.internal.common.util.HTTextUtils;
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import io.branch.referral.Branch;
 import io.fabric.sdk.android.Fabric;
@@ -44,6 +46,13 @@ public class MyApplication extends Application {
 
     private static MyApplication mInstance;
 
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        MyApplication application = (MyApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -55,7 +64,7 @@ public class MyApplication extends Application {
             return;
         }
 
-        LeakCanary.install(this);
+        refWatcher = LeakCanary.install(this);
 
         try {
             if (!HTTextUtils.isEmpty(ApiKey.getApiKey(this))) {
